5b43c85bb6718a931f9842eb63bef1cbc8ab2020
SatyaVarun29 <satyavarun87@gmail.com>
2025-11-11 22:12:24 +0530

    add

--- Backend/models/usermodels.js
+++ Backend/models/usermodels.js
@@ -27,8 +27,12 @@ userSchema.pre('save', async function (next) {
 
     const salt=await bcrypt.genSalt(10)
     this.password=await bcrypt.hash(this.password,salt)
-    
+
 })
+
+userSchema.methods.matchPassword=async function (enteredpassword) {
+    return await bcrypt.compare(enteredpassword, this.password)
+}
 
 const User=mongoose.model('User',userSchema)
 
