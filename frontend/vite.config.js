1e6d0b856e7e8d3eaa093d82632f83a560a499eb
SatyaVarun29 <satyavarun87@gmail.com>
2025-12-11 22:13:57 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port:5000,
-  proxy:{
-    '/api':{
-      target:'http://localhost:5000',
-      changeOrigin:true
+  server:{
+
+    port:3000,
+    proxy:{
+      '/api':{
+        target:'http://localhost:5000',
+        changeOrigin:true
+      }
     }
   }
 })
