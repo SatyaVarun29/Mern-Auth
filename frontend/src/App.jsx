68de6ce08340a5445b71c42916fa3a2ae7d60753
SatyaVarun29 <satyavarun87@gmail.com>
2025-12-14 22:16:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/header";
 import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 
 
@@ -10,7 +11,10 @@ const App = () => {
     <>
      
       <Header/>
+      <Container className="my-2">
+
       <Outlet/>
+      </Container>
     </>
   );
 };
