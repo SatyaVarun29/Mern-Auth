8b2dc91040ef4e4feb296ef251601c0b10562691
SatyaVarun29 <satyavarun87@gmail.com>
2026-02-11 21:07:58 +0530

    Update Registerscreen.jsx

--- frontend/src/screen/Registerscreen.jsx
+++ frontend/src/screen/Registerscreen.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Col, Row, Form, Button } from "react-bootstrap";
 import FormContainer from "../components/formcontainer";
